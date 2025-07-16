f873293233b0ab217d8e5a7696c4922a9e16e6b9
Preeti sharma <ps953682@gmail.com>
2025-07-16 12:28:23 +0530

    change cart page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -35,10 +35,10 @@ const Cart = () => {
       {products.length > 0 ? (
         <div className="pb-20">
           <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
-            <h2 className="col-span-2">Productt</h2>
+            <h2 className="col-span-2">Product</h2>
             <h2>Price</h2>
             <h2>Quantity</h2>
-            <h2>Sub Totalll</h2>
+            <h2>Sub Total</h2>
           </div>
           <div className="mt-5">
             {products.map((item) => (
