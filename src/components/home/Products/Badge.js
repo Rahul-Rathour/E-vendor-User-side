ff49bdf97da1cee88e97d8b28f8eada640484334
Rahul-Rathour <rahulrathor0988@gmail.com>
2026-09-05 13:22:12 +0530

    button colos

--- src/components/home/Products/Badge.js
+++ src/components/home/Products/Badge.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Badge = ({ text }) => {
   return (
-    <div className=" w-[92px] h-[35px] text-white flex justify-center items-center text-base font-semibold hover:bg-#9F2B68 duration-300 cursor-pointer"style={{ backgroundColor: "#9F2B68" }}>
+    <div className="w-[92px] h-[35px] text-white flex justify-center items-center text-base font-semibold bg-brandColor hover:bg-[#9F2B68] duration-300 cursor-pointer">
       {text}
     </div>
   );
